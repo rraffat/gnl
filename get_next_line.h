7cb35f005ba5879f3b6dbdd3f3069b2a859455e1
Ragaie Raffat <rraffat@lab3r5s7.42abudhabi.ae>
2022-02-04 20:04:12 +0400

    hi

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rraffat <rraffat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 07:23:21 by rraffat           #+#    #+#             */
+/*   Updated: 2022/01/28 07:20:57 by rraffat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+//char	*ft_strdup(const char *s1);
+
+char	*ft_get_line(char *save);
+char	*ft_save(char *save);
+char	*ft_read_and_save(int fd, char *save);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rraffat <rraffat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 16:04:16 by rraffat           #+#    #+#             */
+/*   Updated: 2022/01/28 16:07:41 by rraffat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t			ft_strlen(char *str);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+
+char			*get_next_line(int fd);
+
+char			*ft_get_line(char *save);
+char			*ft_save(char *save);
+char			*ft_read_and_save(int fd, char *save);
+
+#endif
